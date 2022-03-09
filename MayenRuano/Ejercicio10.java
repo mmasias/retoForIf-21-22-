60760b76626f006c0907f706e3adab5317e51231
PamelaMayen <73257728+PamelaMayen@users.noreply.github.com>
2022-03-09 08:19:15 +0100

    Ejercicios reto forif

---
+++ MayenRuano/Ejercicio10.java
@@ -0,0 +1,12 @@
+package MayenRuano;
+
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 21; i++){
+          for(int j = 1; j <= 21; j++){
+                System.out.print(" - ");
+          }
+          System.out.println();
+        }
+    }
+}
