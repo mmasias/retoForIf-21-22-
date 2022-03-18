06f407ba8ac8d073f4bdc6f3ce6c39ecfb70cc2a
David Montenegro <68624139+Dmon4516@users.noreply.github.com>
2022-03-18 11:42:33 +0100

    Listo

--- Code/class20.java
+++ Code/class20.java
@@ -6,12 +6,12 @@ public class class20{
             //r^2=a^2+b^2;
             // r^2<=x^2+y^2;
             //r^2>=x^2+y^2; Hay que buscar como desplazarlo 
-            if (j==i){
-                System.out.print(" . ");
+            if(Math.pow(4, 2)>=Math.pow(i-11, 2)+ Math.pow(j-11, 2)||(Math.pow(5, 2)+1<=Math.pow(i-11, 2)+ Math.pow(j-11, 2))){
+                System.out.print(" 0 ");
                 
                 
             } else {
-                System.out.print(" 0 ");
+                System.out.print(" . ");
             }
         }
          System.out.println();
